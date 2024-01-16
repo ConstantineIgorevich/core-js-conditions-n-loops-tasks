d0d556b1d0ba872b35dff1cf9c0610f10e02370b
Kanstantsin Davydovich <kostiarabota@gmail.com>
2024-01-16 21:29:51 +0300

    feat: add sortByAsc function with bubble sort

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -411,8 +411,20 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const result = arr;
+  let i = result.length - 1;
+  while (i > 0) {
+    for (let j = 0; j < i; j += 1) {
+      if (result[j] > result[j + 1]) {
+        const temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+    i -= 1;
+  }
+  return result;
 }
 
 /**
